f4b009b0c28d2cd3c1ed8b977b0ad9d0788e8459
Zaher Salem <zahersalem46@gmail.com>
2025-06-25 12:48:55 +0300

    Add Code Genration .tt

---
+++ MOJ.ProductManagement.Domain/MOJ.ProductManagement.Web.Client.Angular/src/app/core/generated/generated-enums.ts
@@ -0,0 +1,20 @@
+// Auto-generated TypeScript enums
+// Generated on 2025-06-25 12:54
+// DO NOT EDIT - Regenerate from T4 template instead
+
+export enum QuantityPerUnit {
+    Kilo = 10,
+    Box = 11,
+    Can = 12,
+    Liter = 13,
+    Bottle = 14,
+}
+
+export const QuantityPerUnitLabels: Record<QuantityPerUnit, string> = {
+    [QuantityPerUnit.Kilo]: 'Kilo',
+    [QuantityPerUnit.Box]: 'Box',
+    [QuantityPerUnit.Can]: 'Can',
+    [QuantityPerUnit.Liter]: 'Liter',
+    [QuantityPerUnit.Bottle]: 'Bottle',
+};
+
