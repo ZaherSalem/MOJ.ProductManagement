fb85614fe837d77441981a426c1a4e661cf47001
Zaher Salem <zahersalem46@gmail.com>
2025-06-25 20:07:53 +0300

    Add lastOrder Date Col to product table

---
+++ MOJ.ProductManagement.Application/DTOs/Supplier/LargestSupplierDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MOJ.ProductManagement.Application.DTOs.Supplier
+{
+    public class LargestSupplierDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
---
+++ MOJ.ProductManagement.Infrastructure/Migrations/20250625170255_add_lastOrderDate_col.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MOJ.ProductManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_lastOrderDate_col : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastOrderDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductId",
+                table: "Products",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Products_ProductId",
+                table: "Products",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Products_ProductId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LastOrderDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Products");
+        }
+    }
+}
